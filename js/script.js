7f5b4cbedb16ff6987fa4a5a0e172e3510b0e0da
ShiedaGX <ormis0620@mail.com>
2024-05-20 20:34:45 +0700

    Second commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
-// ini js 
-// iya ini js
+function luas() {
+    alas = Number(document.getElementById("alas").value);
+    tinggi = Number(document.getElementById("tinggi").value);
+    Number(document.getElementById("luas").innerHTML = (tinggi / 2) * alas);
+}
+
+function keliling() {
+    a = Number(document.getElementById("a").value);
+    b = Number(document.getElementById("b").value);
+    c = Number(document.getElementById("c").value);
+    Number(document.getElementById("keliling").innerHTML = a + b + c);
+}
